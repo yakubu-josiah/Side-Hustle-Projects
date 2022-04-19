fa11a707ccecf4eeca734f88859a53b997e60951
Cittih-Joey <54555594+Cittih-Joey@users.noreply.github.com>
2022-04-18 21:39:27 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import Head from "./Head";
+import Experience from "./Experience";
+
+function App() {
+  // const title = "Welcome to the New Blog";
+  // const like = 50;
+  return (
+    <div className="App">
+      <Head />
+      <div className="contet">
+      <Experience/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Head.js
@@ -0,0 +1,24 @@
+const Head = () => {
+    return ( 
+       <div className="Head">
+            <header>
+                <div id="head">
+                    <p>Yakubu Josiah </p>
+                    <p><span id="Jb-Tl">Software Developer</span></p>
+                </div>
+                
+            </header>
+            <nav className="nav">
+                <ul>
+                <li><a href="/">GitHub </a></li>
+                    <li><a href="/">Linkden ME</a></li>
+                    <li><a href="mailto:jossysmothest007@gmail.com">YaQuB@gmail.com</a></li>
+                    <li>Phone (+234) 807 735 000</li>
+                    <li>Address: <em>52 Yabatech Staff Quartez, Yaba, Lagos, Nigeria.</em></li>
+                </ul>
+            </nav>
+       </div>
+    );
+}
+ 
+export default Head;
